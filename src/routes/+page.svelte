b21d206c977774ae7d886b5507810899bccedca1
Deepak Kumar <deepakkumardash313@gmail.com>
2026-01-14 02:24:04 +0530

    Setup Shadcn and sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Button from "$lib/components/ui/button/button.svelte";
+	import { MoonIcon, SunIcon } from "@lucide/svelte";
+
+</script>
+
+<h1 class="text-center text-purple-400">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button onclick={() => {}} variant="outline" size="icon">
+  <SunIcon
+    class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+  />
+  <MoonIcon
+    class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+  />
+  <span class="sr-only">Toggle theme</span>
+</Button>
